cef085371ca5355b65b675510d7f8bd83d7a3507
tiagoskaterock <tiagolemespalhano@gmail.com>
2024-01-10 18:27:50 -0300

    Styles updated on footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,18 +1,10 @@
-<br>
-<hr>
-<br>
-<style>
-	.footer {
-		text-align: right;
-		margin-right: 20px;
-		height: auto;
-		color: black;
-		font-family: "Gill Sans", "Gill Sans MT", "Myriad Pro","sans-serif";
-	}
-</style>
-
 <div class="footer">
-	<p> &copy; 2020 MANZI GHILAIN. S/No. WS1805005. Click here for
-		<a href="admin/adminlogin.php" target="_blank">Admin Login</a>
+	<p> &copy; <?= date('Y') ?> MANZI GHILAIN. S/No. WS1805005. 
+		<a 
+			href="admin/adminlogin.php" 
+			target="_blank"
+			title="Login as Admin">
+			Click here for Admin Login
+		</a>
 	</p>
 </div>
